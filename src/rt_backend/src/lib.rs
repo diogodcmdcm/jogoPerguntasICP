88506ea855e92d7b567f90b9344371a0df40ddd4
diogodcmdcm <diogo.micheluzzi@gmail.com>
2024-09-23 11:47:41 -0300

    Adicionado o projeto

--- src/rt_backend/src/lib.rs
+++ src/rt_backend/src/lib.rs
@@ -1,4 +1,51 @@
+
+
+// Array de 4 itens de texto (strings literais)
+const PERGUNTAS: [&str; 10] = ["Qual é a capital da França?", 
+                            "Qual é o maior planeta do Sistema Solar?", 
+                            "Quem escreveu Dom Casmurro?", 
+                            "Qual é o elemento químico com o símbolo O?",
+                            "Em que ano o homem pisou na Lua pela primeira vez?",
+                            "Qual é o continente mais populoso do mundo?",
+                            "Qual é o maior órgão do corpo humano?",
+                            "Qual é a moeda oficial do Japão?",
+                            "Quem pintou A Última Ceia?",
+                            "Qual é a fórmula química da água?"];                                                                     
+                           
+const OPCOES: [&str; 10] = ["Londres;Paris;Berlim;Roma", 
+                            "Terra;Marte;Júpiter;Saturno", 
+                            "Machado de Assis;José de Alencar;Jorge Amado;Clarice Lispector", 
+                            "Ouro;Oxigênio;Ósmio;Oxido", 
+                            "1965;1969;1971;1975",
+                            "América;Europa;Ásia;África",
+                            "Coração;Pulmão;Pele;Fígado",
+                            "Yuan;Won;Iene;Dólar",
+                            "Michelangelo;Leonardo da Vinci;Rafael;Van Gogh",
+                            "H2O;CO2;O2;H2O2"];
+
+const RESPOSTAS: [u64; 10] = [2, 3, 1, 2, 2, 3, 3, 3, 2, 1];
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn get_pergunta(index: u64) -> String {
+    format!("{}", PERGUNTAS[index as usize])  
+
 }
+
+#[ic_cdk::query]
+fn get_respostas(index: u64) -> String {
+
+    format!("{}", OPCOES[index as usize])  
+
+}
+
+#[ic_cdk::query]
+fn validar_resposta(index: u64, res: u64) -> String {
+    
+    let mut ponto: u64 = 0;
+    if RESPOSTAS[index as usize] == res {            
+        ponto = 100;            
+    }
+
+    format!("{}", ponto)
+}
+
--- src/rt_frontend/src/App.jsx
+++ src/rt_frontend/src/App.jsx
@@ -1,29 +1,126 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { rt_backend } from 'declarations/rt_backend';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [pergunta, setPergunta] = useState('');
+  const [indexPergunta, setIndexPergunta] = useState(0);
+  const [pontos, setPontos] = useState(0);
+  const [jogoFinalizado, setJogoFinalizado] = useState(false);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    rt_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
+  const [opcao1, setOpcao1] = useState('');
+  const [opcao2, setOpcao2] = useState('');
+  const [opcao3, setOpcao3] = useState('');
+  const [opcao4, setOpcao4] = useState('');
+
+  const [selectedOption, setSelectedOption] = useState(1);
+
+  useEffect(() => {
+    rt_backend.get_pergunta(0).then((perg) => {
+      setPergunta(perg);
     });
-    return false;
+
+    rt_backend.get_respostas(BigInt(0)).then((resp) => {        
+      const arrayRespostas = resp.split(';');
+      setOpcao1(arrayRespostas[0]);
+      setOpcao2(arrayRespostas[1]);        
+      setOpcao3(arrayRespostas[2]);
+      setOpcao4(arrayRespostas[3]);
+    });
+
+  }, []); // 
+
+  async function proximaPergunta(){
+    
+    let p = await rt_backend.validar_resposta(BigInt(indexPergunta), BigInt(selectedOption));
+    setPontos(pontos+Number(p));      
+     
+     let i = indexPergunta+1;      
+
+     if(i<=9){
+        setIndexPergunta(i);         
+        rt_backend.get_pergunta(BigInt(i)).then((perg) => { setPergunta(perg); });
+
+        rt_backend.get_respostas(BigInt(i)).then((resp) => {        
+          const arrayRespostas = resp.split(';');
+          setOpcao1(arrayRespostas[0]);
+          setOpcao2(arrayRespostas[1]);        
+          setOpcao3(arrayRespostas[2]);
+          setOpcao4(arrayRespostas[3]);
+        });      
+     } else {  
+      setJogoFinalizado(true);
+     }
+
   }
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+
+      {jogoFinalizado && <div><h2>Jogo Finalizado!</h2> <label>Sua pontuação foi de {pontos}</label>
+          <br /><br /><button style={{ fontSize: '20px', padding: '10px 20px' }} >Jogar Novamente</button>
+        </div> } 
+
+      {!jogoFinalizado && <div style={{margin: '20px' }} >
+          <h2>Jogo do milhão</h2>      
+          <label>Responda as perguntas abaixo:</label>          
+          <p style={{color: 'blue'}} ><h2>{pergunta}</h2></p>
+          <div>
+              <b><label htmlFor="pergunta" >Escolha a sua resposta:</label></b>
+              <br />
+              <br />
+              <div>
+                <input
+                  type="radio"
+                  name="options"
+                  value="1"
+                  checked={selectedOption === '1'}
+                  onChange={handleOptionChange}
+                />
+                <label>{opcao1}</label>
+              </div>
+
+              <div>
+                <input
+                  type="radio"
+                  name="options"
+                  value="2"
+                  checked={selectedOption === '2'}
+                  onChange={handleOptionChange}
+                />
+                <label>{opcao2}</label>
+              </div>
+
+            <div>
+              <input
+                type="radio"
+                name="options"
+                value="3"
+                checked={selectedOption === '3'}
+                onChange={handleOptionChange}
+              />
+              <label>{opcao3}</label>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                name="options"
+                value="4"
+                checked={selectedOption === '4'}
+                onChange={handleOptionChange}
+              />
+              <label>{opcao4}</label>
+            </div>
+            <br />
+            <button onClick={ ()=> {proximaPergunta()}} style={{ fontSize: '20px', padding: '10px 20px' }} >Enviar Resposta</button>
+         
+          </div>
+      </div> }
+      
     </main>
   );
 }
